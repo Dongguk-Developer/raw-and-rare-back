3735e549875d0ee536344c500b9f74635765c04a
kimehyun <lh7721004@gmail.com>
2025-04-30 17:34:54 +0900

    feat: 백엔드 환경 세팅

---
+++ src/main/java/com/example/raw_and_rare/RawAndRareApplication.java
@@ -0,0 +1,13 @@
+package com.example.raw_and_rare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawAndRareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RawAndRareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/raw_and_rare/RawAndRareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raw_and_rare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawAndRareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bfae10ec8fb0387a33d840bd7e878f29035071c5
김이현 <lh7721004@gmail.com>
2025-05-07 16:51:05 +0900

    feat : entity 작성 및 종속성 변경

---
+++ src/main/java/com/example/raw_and_rare/entity/auth/OAuth.java
@@ -0,0 +1,38 @@
+package com.example.raw_and_rare.entity.auth;
+
+import com.example.raw_and_rare.entity.auth.user.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.Instant;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OAuth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "oauth_id",unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @Column(nullable = false)
+    private String token;//액세스 토큰
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @Column(nullable = false)
+    private Long kakaoId;
+
+}
---
+++ src/main/java/com/example/raw_and_rare/entity/auth/user/UserStatus.java
@@ -0,0 +1,7 @@
+package com.example.raw_and_rare.entity.auth.user;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE,
+    BLOCKED
+}
